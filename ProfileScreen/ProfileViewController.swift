69a7d8bc0feca8ae81b771611c85dbc04e74458c
ulichev <andreyulichev@gmail.com>
2025-05-28 23:38:33 +0300

    Выполнена 2 тема из спринта 9

---
+++ ProfileScreen/ProfileViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+  
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabelView: UILabel!
+    @IBOutlet weak var accountLabelView: UILabel!
+    @IBOutlet weak var descriptionLabelView: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func logoutButtonTapped(_ sender: Any) {
+        // вписать сюда дальше код
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
